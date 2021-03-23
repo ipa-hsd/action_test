10b7fafc06a2b0f50943e678d58befb9bf7520b9
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-03-23 11:24:37 +0100

    use ROS_DISTRO var

--- build_bridge.sh
+++ build_bridge.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -e
 
-unset $ROS_DISTRO
-echo $ROS1_DISTRO
-env
-echo $ROS2_DISTRO
-echo "Hello world"
-source /opt/ros/rolling/setup.bash
+source /opt/ros/$ROS_DISTRO/setup.bash
 colcon build --symlink-install
