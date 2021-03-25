d536145712924eb8ac6437c7ed76300c21530b2c
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-03-25 21:12:55 +0100

    created dummy package

--- build_bridge.sh
+++ build_bridge.sh
@@ -2,7 +2,7 @@
 set -e
 
 source /opt/ros/$ROS_DISTRO/setup.bash
-mkdir -p ~/colcon_ws/src
-cd ~/colcon_ws/
-cp -r $GITHUB_WORKSPACE ~/colcon_ws/src
+# mkdir -p ~/colcon_ws/src
+# cd ~/colcon_ws/
+# cp -r $GITHUB_WORKSPACE ~/colcon_ws/src
 colcon build --symlink-install
