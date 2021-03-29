68441009440239e15f4b0455b274746dcddb852d
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-03-29 15:41:46 +0200

    install deps

--- build_bridge.sh
+++ build_bridge.sh
@@ -5,4 +5,5 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 # mkdir -p ~/colcon_ws/src
 # cd ~/colcon_ws/
 # cp -r $GITHUB_WORKSPACE ~/colcon_ws/src
+rosdep install --from-paths . --ignore-src -r -y
 colcon build --symlink-install
