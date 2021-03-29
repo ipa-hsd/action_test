407216dc916c7e746bc773abf917fb0b6264cdae
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-03-29 15:43:25 +0200

    update rosdep

--- build_bridge.sh
+++ build_bridge.sh
@@ -5,5 +5,6 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 # mkdir -p ~/colcon_ws/src
 # cd ~/colcon_ws/
 # cp -r $GITHUB_WORKSPACE ~/colcon_ws/src
+rosdep update
 rosdep install --from-paths . --ignore-src -r -y
 colcon build --symlink-install
