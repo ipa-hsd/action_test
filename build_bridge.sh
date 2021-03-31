71319cf21ca043a31692b39b9978d00e1b7464ee
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-03-31 15:09:37 +0200

    export ccache path

--- build_bridge.sh
+++ build_bridge.sh
@@ -2,9 +2,9 @@
 set -e
 
 source /opt/ros/$ROS_DISTRO/setup.bash
-# mkdir -p ~/colcon_ws/src
-# cd ~/colcon_ws/
-# cp -r $GITHUB_WORKSPACE ~/colcon_ws/src
+mkdir -p ~/colcon_ws/src
+cd ~/colcon_ws/
+cp -r $GITHUB_WORKSPACE ~/colcon_ws/src
 rosdep update
 rosdep install --from-paths . --ignore-src -r -y
 colcon build --symlink-install
