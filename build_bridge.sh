49e5ceb62e51ead8a1d0fe232ff013c589da81eb
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2021-04-02 11:46:43 +0200

    bring back the tests

--- build_bridge.sh
+++ build_bridge.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
+if [ -n "$CCACHE_DIR" ]; then
+    export PATH="/usr/lib/ccache:$PATH"
+fi
+
 source /opt/ros/$ROS_DISTRO/setup.bash
-# mkdir -p ~/colcon_ws/src
-# cd ~/colcon_ws/
-# cp -r $GITHUB_WORKSPACE ~/colcon_ws/src
+mkdir -p ~/colcon_ws/src
+cd ~/colcon_ws/
+cp -r $GITHUB_WORKSPACE ~/colcon_ws/src
 rosdep update
-rosdep install --from-paths . --ignore-src -r -y
+rosdep install --from-paths src --ignore-src -r -y
 colcon build --symlink-install --event-handlers console_direct+
